3ac0d95a08b2da5354d876e2d4be825cd4e531b9
Suraj Taru <surajtaru364@gmail.com>
2025-10-02 20:35:59 +0530

    leetcode 14

---
+++ EASY/LongestCommonPrefix.java
@@ -0,0 +1,55 @@
+/*
+    following program i solved on my own. I was having lot of difficulty trying to solve this problem
+    
+    Date : Oct 02, 2025
+    Time : 8:00 PM
+    Place: Shreyan Pg viman nagar pune
+    
+    problem : 
+    Input: strs = ["flower","flow","flight"]
+    Output: "fl"
+
+    Example 2:
+    Input: strs = ["dog","racecar","car"]
+    Output: ""
+*/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = "";
+        boolean flag, helper = true;
+        if (strs.length == 0) {
+            return "";
+        }
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        int i, j;
+        for (i = 0; i < strs[0].length(); i++) {
+            flag = true;
+            for (j = 1; j < strs.length; j++) {
+                // When this conditon fails no need to go any further
+                helper = i < strs[j].length();
+                if (!helper) {
+                    return prefix;
+                }
+
+                if (strs[0].charAt(i) != strs[j].charAt(i)) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) {
+                prefix = prefix + strs[0].charAt(i);
+            } else {
+                break;
+            }
+        }
+        return prefix;
+    }
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # leetcode-solved
 This repository has all the problems I have solved and also the problems I am solving. Throughout the months I have solved many problems. 
+
+### Longest common prefix
+Date : Oct 02, 2025
+Time : 8:00 PM
+Place: Shreyan Viman
+
+
